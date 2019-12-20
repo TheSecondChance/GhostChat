1aaeab8225557f4a32f57825efe23ca0f1d8f03e
TheSecondChance <roenko.dv@gmail.com>
2019-12-20 14:38:17 +0700

    Added generic repository for CRUD operations

---
+++ GhostChat.Data/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GhostChat.Data
+{
+    public interface IRepository<Type> where Type : class
+    {
+        List<Type> GetAll();
+        Type GetById(object id);
+        void Add(Type entity);
+        void Update(Type entity);
+        void Put(Type entity);
+        void Remove(Type entity);
+    }
+}
---
+++ GhostChat.Data/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GhostChat.Data
+{
+    public class Repository<Type> : IRepository<Type> where Type : class
+    {
+        private readonly ApplicationContext dbContext;
+        private DbSet<Type> entities;
+
+        public Repository(ApplicationContext dbContext)
+        {
+            this.dbContext = dbContext;
+            entities = dbContext.Set<Type>();
+        }
+
+        public List<Type> GetAll()
+        {
+            return entities.ToList();
+        }
+
+        public Type GetById(object id)
+        {
+            return entities.Find(id);
+        }
+
+        public void Add(Type entity)
+        {
+            entities.Add(entity);
+            dbContext.SaveChanges();
+        }
+
+        public void Update(Type entity)
+        {
+            entities.Update(entity);
+            dbContext.SaveChanges();
+        }
+
+        public void Put(Type entity)
+        {
+            var entry = dbContext.Entry(entity);
+            switch (entry.State)
+            {
+                case EntityState.Detached:
+                    Update(entity);
+                    break;
+                case EntityState.Modified:
+                    Add(entity);
+                    break;
+                case EntityState.Added:
+                    Add(entity);
+                    break;
+            }
+        }
+
+        public void Remove(Type entity)
+        {
+            entities.Remove(entity);
+            dbContext.SaveChanges();
+        }
+    }
+}
